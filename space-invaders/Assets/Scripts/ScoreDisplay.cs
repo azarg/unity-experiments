998d79d7abb3db6f431e56f2c9cfb1ccc146f8b4
azarg <azar.gurbanov@gmail.com>
2023-08-05 23:24:08 -0500

    a bit more stylized score display

--- space-invaders/Assets/Scripts/ScoreDisplay.cs
+++ space-invaders/Assets/Scripts/ScoreDisplay.cs
@@ -1,15 +1,15 @@
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro;
 
 public class ScoreDisplay : MonoBehaviour {
     [SerializeField] IntVariable score;
-    [SerializeField] private Text scoreText;
+    [SerializeField] private TextMeshProUGUI scoreText;
 
     private void Start () {
         score.OnValueChanged += Score_OnValueChanged;
     }
 
     private void Score_OnValueChanged() {
-        scoreText.text = $"score:" + score.GetValue().ToString("000");
+        scoreText.text = score.GetValue().ToString("000");
     }
 }
